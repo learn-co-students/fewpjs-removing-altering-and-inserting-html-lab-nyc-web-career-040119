feb34ab479c77352597089ec9d026db239d88128
danfyfe <fyfedaniel@gmail.com>
2019-05-13 14:01:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+let element = document.getElementById('main')
+element.remove()
+
+let newHeader = document.createElement('h1')
+
+document.body.appendChild(newHeader)
+newHeader = document.querySelector("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
