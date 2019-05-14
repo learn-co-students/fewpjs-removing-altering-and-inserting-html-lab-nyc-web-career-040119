c6d164433d1859e6359cac4f2e848fa3c2b9bf11
Daviicode <d.ydzdavid@yahoo.com>
2019-05-13 20:07:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main = document.querySelector(`#main`)
+main.remove()
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'Victory'
+newHeader.innerHTML = "Your-Name is the Champion"
